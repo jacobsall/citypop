2f366e00108d01b459cd9fbedf5b1b505e5a21cb
Jacob Säll Nilsson <jacob.e.s.nilsson@gmail.com>
2021-02-20 18:42:05 +0100

    imported alert from material-ui for nicer errors

--- src/components/ErrorBox.js
+++ src/components/ErrorBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Alert } from '@material-ui/lab';
 
 export default function ErrorBox(props) {
     return (
         <div>
-            {props.message}
+            <Alert severity="error">{props.message}</Alert>
         </div>
     )
 }
